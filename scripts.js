0097f9a6d06342ebc431960da413db786d1357f5
Una Kamilla <unakamilla@gmail.com>
2019-11-04 18:36:55 +0000

    ugh

--- scripts.js
+++ scripts.js
@@ -15,6 +15,7 @@ const text = (() => {
     _form.addEventListener('submit', formHandler);
 
     // TODO láta hluti í _items virka
+
   }
 
   function formHandler(e) {
@@ -25,6 +26,7 @@ const text = (() => {
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+    e.setAttribute('class', 'item--done');
   }
 
   // event handler fyrir það að breyta færslu
@@ -37,6 +39,21 @@ const text = (() => {
 
   // fall sem sér um að bæta við nýju item
   function add(value) {
+    var newItem = document.createElement('li');
+    newItem.setAttribute('class', 'item');
+    const newInput = newItem.createElement('input')
+    newInput.setAttribute('type', 'checkbox');
+    newInput.setAttribute('class', 'item__checkbox');
+    const newSpan = newItem.createElement('span');
+    newSpan.setAttribute('class', 'item__text');
+    const spanText = document.createTextNode(value);
+    newSpan.appendChild(spanText);
+    const newButton = newItem.createElement('button');
+    newButton.setAttribute('class', 'button');
+    const buttonText = document.createTextNode('Eyða');
+    newButton.appendChild(buttonText);
+
+    document.body.main.ul.appendChild(newItem); // wot
   }
 
   // event handler til að eyða færslu
