703b8a2a67bcf01e0e817ae1ccaf54b15c83352f
Una Kamilla <unakamilla@gmail.com>
2019-11-07 13:35:22 +0000

    commit fukin virkar ekki, vantar líka deleteItem

--- scripts.js
+++ scripts.js
@@ -3,10 +3,6 @@ const ENTER_KEYCODE = 13;
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.form');
   const items = document.querySelector('.items');
-  buttons = document.getElementsByClassName("item__button");
-  texts = document.getElementsByClassName("item__text");
-  boxes = document.getElementsByClassName("item__checkbox");
-  // ^ hvar eiga þessir eiginlega að vera ???
 
   text.init(form, items);
 });
@@ -19,16 +15,16 @@ const text = (() => {
     _form.addEventListener('submit', formHandler);
 
     // TODO láta hluti í _items virka
-    // buttons = document.getElementsByClassName("item__button");
+    buttons = document.getElementsByClassName("item__button");
     for (i=0; i<buttons.length; i++) {
       buttons[i].addEventListener('click', deleteItem);
     }
-    // texts = document.getElementsByClassName("item__text");
+    texts = document.getElementsByClassName("item__text");
     for (i=0; i<buttons.length; i++) {
       texts[i].addEventListener('click', edit);
-      texts[i].addEventListener('keyup', commit) ;
+      texts[i].addEventListener('keydown', commit);
     }
-    // boxes = document.getElementsByClassName("item__checkbox");
+    boxes = document.getElementsByClassName("item__checkbox");
     for (i=0; i<boxes.length; i++) {
       boxes[i].addEventListener('click', finish);
     }
@@ -39,7 +35,6 @@ const text = (() => {
     task = document.querySelector('.form__input').value;
     add(task);
     document.querySelector('.form__input').value = "";
-    console.log('halló heimur');
   }
 
   // event handler fyrir það að klára færslu
@@ -49,13 +44,26 @@ const text = (() => {
 
   // event handler fyrir það að breyta færslu
   function edit(e) {
-    // breyta span í input type text
-    // setja texta sem var fyrir í value
+    this.removeEventListener('click', edit);
+    task = this.parentNode.querySelector('.item__text').textContent;
+    const changeTask = document.createElement('input');
+    changeTask.setAttribute('type', 'text');
+    changeTask.setAttribute('class', 'item__edit')
+    changeTask.value = task;
+    this.parentNode.querySelector('.item__text').style.display = 'none';
+    this.parentNode.querySelector('.item__button').style.display = 'none';
+    this.parentNode.appendChild(changeTask);
+    changeTask.focus();
   }
 
   // event handler fyrir það að klára að breyta færslu
-  function commit(e) {
-    // if (e.keyCode === 13) {}
+  function commit(e) { //verkar á span item__text undir li item
+    if (e.keyCode == ENTER_KEYCODE) {
+      task = this.value;
+      this.parentNode.querySelector('.item__edit').remove();
+      this.parentNode.querySelector('.item__text').textContent = task;
+      this.parentNode.querySelector('.item__text').style.display = 'block';
+    }
   }
 
   // fall sem sér um að bæta við nýju item
@@ -66,16 +74,20 @@ const text = (() => {
     newInput.setAttribute('type', 'checkbox');
     newInput.setAttribute('class', 'item__checkbox');
     newItem.appendChild(newInput);
+    newInput.addEventListener('click', finish);
     const newSpan = document.createElement('span');
     newSpan.setAttribute('class', 'item__text');
     newItem.appendChild(newSpan);
     const spanText = document.createTextNode(value);
     newSpan.appendChild(spanText);
+    newSpan.addEventListener('click', edit);
+    newSpan.addEventListener('keyup', commit);
     const newButton = document.createElement('button');
     newButton.setAttribute('class', 'item__button');
     newItem.appendChild(newButton);
     const buttonText = document.createTextNode('Eyða');
     newButton.appendChild(buttonText);
+    newButton.addEventListener('click', deleteItem);
 
     items.appendChild(newItem); // wot
   }
